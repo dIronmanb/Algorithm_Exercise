6b7823567dc408c57540aa86fd06429e022936aa
heeyang <huiyang1022@naver.com>
2022-02-17 20:08:10 +0900

    토마토 끝

--- 7576_토마토_solution.py
+++ 7576_토마토_solution.py
@@ -1,53 +1,50 @@
 import sys
 from collections import deque
 read = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
+field = []
+days = 1
+ripe_tomatoes = deque()
 
-
+# input information
 m, n = [int(i) for i in read().split()]
-graph = []
-visited = []
-empty = 0
+for i in range(n):
+    field.append([int(a) for a in read().split()])
 
+    # extract ripe tomatoes
+    for j in range(len(field[i])):
+        if field[i][j] == 1:
+            ripe_tomatoes.append([i, j]) 
+
+
+
+dx = [1, -1,  0,  0]
+dy = [0,  0,  1, -1]
+days = 0
+
+
+while True:
+    if len(ripe_tomatoes) == 0:
+        break
+    x,y = ripe_tomatoes.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and field[nx][ny] == 0:
+            field[nx][ny] = field[x][y] + 1
+            ripe_tomatoes.append([nx,ny])
+    
+
+flag = False
 
 for i in range(n):
-    graph.append([int(a) for a in read().split()])
-    visited.append([0]*m)
-    # Sum empty spaces
-    empty += graph[i].count(-1)
+    for j in range(m):
+        if field[i][j] == 0:
+            flag = True
+            break
 
-
-days = 0
-pre_tomatoes = 0
-while True:
-    present_tomatoes = 0
-    for i in range(n):
-        for j in range(m):
-            # If the tomatoes are ripe at that location,
-            if graph[i][j] == 1:
-                present_tomatoes += 1
-                bfs(i,j)
-
-    print('\n')  
-    for i in range(n):
-        print(graph[i])
-
-    # If there is no difference in the number of ripe tomatoes from the previous day and today
-    if(present_tomatoes == pre_tomatoes):
-        break
-
-    # When all the tomatoes have been already ripe
-    elif(m * n + empty - present_tomatoes == 0):
-        break
-
-    else:
-        pre_tomatoes = present_tomatoes
-        days += 1
-
-# When all the tomatoes are ripe
-if(m * n - empty - present_tomatoes == 0):
-      print(days)
-# Otherwise
-else:
+if flag:
     print(-1)
+else:
+    print(max([max(field[i]) for i in range(n)]) - 1)
+    
