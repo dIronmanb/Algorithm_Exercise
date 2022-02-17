b5702fcac5619cb48e29f14dfef147ff80ff5e6e
heeyang <huiyang1022@naver.com>
2022-02-17 09:57:19 +0900

    토마토 푸는 중 - Recursion Error

--- 7576_토마토.py
+++ 7576_토마토.py
@@ -4,40 +4,61 @@ import sys
 read = sys.stdin.readline
 sys.setrecursionlimit(10000)
 
-count = 0
 
-def dfs(x: int, y: int)->int:
-    global count
-    blocked_count = 0
+def dfs(x: int, y: int)->None:
     dx = [1, -1,  0,  0]
     dy = [0,  0,  1, -1]
     
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        # if the position state is 1, adjacent position states convert 0 to 1
-        if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
+        # The tomatoes that are adjacent to each other are ripe.
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
             graph[nx][ny] = 1
-            blocked_count = 0
-            dfs(nx, ny)
-        elif nx < 0 or nx >= m or ny < 0 or ny > n or (0 <= nx < m and 0 <= ny < n and (graph[nx][ny] == -1 or graph[nx][ny] == 1)):
-            blocked_count += 1
-            if blocked_count == 4:
-                return -1
+            
 
-    return count
 
 
 m, n = [int(i) for i in read().split()]
 graph = []
-for _ in range(n):
-    graph.append([int(i) for i in read().split()])
+empty = 0
+
+
 for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            val = dfs(i,j)
+    graph.append([int(a) for a in read().split()])
+    # Sum empty spaces
+    empty += graph[i].count(-1)
+
+days = 0
+pre_tomatoes = 0
+while True:
+    present_tomatoes = 0
+    for i in range(n):
+        for j in range(m):
+            # If the tomatoes are ripe at that location,
+            if graph[i][j] == 1:
+                present_tomatoes += 1
+                dfs(i,j)
+                
     
-print(val)
+    # If there is no difference in the number of ripe tomatoes from the previous day and today
+    if(present_tomatoes == pre_tomatoes):
+        break
+
+    # When all the tomatoes have been already ripe
+    elif(m * n + empty - present_tomatoes == 0):
+        break
+
+    else:
+        pre_tomatoes = present_tomatoes
+        days += 1
+
+# When all the tomatoes are ripe
+if(m * n + empty - present_tomatoes == 0):
+      print(days)
+# Otherwise
+else:
+    print(-1)
 
 
     
