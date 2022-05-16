bd81f87943165fcb42a8cd5a328878d08c9c37be
dIronmanb <huiyang1022@naver.com>
2022-05-16 02:03:40 +0000

    git commit 2573 problem(solving...)

--- BOJ/2573_빙산.py
+++ BOJ/2573_빙산.py
@@ -1,7 +1,10 @@
+'''
+    시간 초과
+    Recursion Error
+'''
+
 import sys
-from copy import deepcopy
 read = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 # 입력
 dx = [-1,  1,  0,  0] 
@@ -10,22 +13,14 @@ y, x = [int(i) for i in read().split()]
 
 iceberg = []
 melted_ice = []
-shape = []
-visited = None
 
-
+# Create and iceberg and melted_iceberg
 for j in range(y):
     iceberg.append([int(i) for i in read().split()])
     melted_ice.append([0 for _ in range(x)])
-    shape.append([1 for _ in range(x)])
 
 
-def renew_ice(visited):
-    for j in range(y):
-        for i in range(x):
-            if iceberg[j][i] == 0:
-                visited[j][i] = 0
-        
+       
 # 빙산이 녹는 과정의 알고리즘
 def melt_ice(iceberg):
     for j in range(1, y-1):
@@ -35,6 +30,7 @@ def melt_ice(iceberg):
                 nx = i + dx[a]
                 
                 if iceberg[ny][nx] == 0:
+                    # 이미 여기서 melted_ice의 정보가 담겨 있다.
                     melted_ice[j][i] += 1
             
     for j in range(y):
@@ -42,19 +38,23 @@ def melt_ice(iceberg):
             iceberg[j][i] -= melted_ice[j][i]
             if iceberg[j][i] < 0:
                 iceberg[j][i] = 0
-            # 녹았던 얼음 초기화
-            melted_ice[j][i] = 0
 
 # dfs로 빙산 개수 카운트
-def dfs(visited, y, x):
-    visited[y][x] = 0
+def dfs(root):
     
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
+    stack = [root] # (y,x) 좌표를 의미함
+
+    while(stack):
+
+        node = stack.pop()
+        melted_ice[node[0]][node[1]] = 0
         
-        if visited[ny][nx] == 1:
-            dfs(visited, ny, nx)
+        for i in range(4):
+            ny = node[0] + dy[i]
+            nx = node[1] + dx[i]
+            
+            if (0 <= nx < x) and (0 <= ny < y) and (melted_ice[ny][nx] != 0):
+                stack.append((ny,nx))
 
 
 loop_flag = True
@@ -63,24 +63,30 @@ while loop_flag:
     iterate += 1
     
     melt_ice(iceberg)
-    renew_ice(shape)
-    visited = deepcopy(shape)
     
     cnt = 0
     for j in range(1, y - 1):
         for i in range(1, x - 1):
-            if visited[j][i] == 1:
-                dfs(visited, j, i)
+            if melted_ice[j][i] != 0:
+                dfs((j, i))
                 cnt += 1
                 
     if cnt == 1:
-        # print("Not splited")
-        pass
-    elif cnt == 0:
-        # print("Completely melted")
-        loop_flag = False
+        iceberg_flag = True
+        for j in range(y):
+            for i in range(x):
+                if iceberg[j][i] != 0:
+                    iceberg_flag = False
+                    break
+                
+        if iceberg_flag:
+            print("Completely melted")
+            break
+        else:
+            print("Not splited")
+        
     else:
-        # print("Splited into {} parts".format(cnt))
+        print("Splited into {} parts".format(cnt))
         break
         
 # print("\n\n")
